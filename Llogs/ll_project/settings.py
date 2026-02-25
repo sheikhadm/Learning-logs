3d308700af96826f68f268f7b70b81922d9cea4b
Abdullahi <abkid04@gmail.com>
2026-02-25 17:53:02 +0100

    static setting

--- Llogs/ll_project/settings.py
+++ Llogs/ll_project/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 LOGIN_REDIRECT_URL = 'learning_logs:index'
 LOGOUT_REDIRECT_URL = 'learning_logs:index'
 LOGIN_URL = 'accounts:login'
