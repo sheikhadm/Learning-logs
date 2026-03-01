31ee822781d2de9c286daf108738759be48ac14c
Abdullahi <abkid04@gmail.com>
2026-03-01 13:16:03 +0100

    n

--- Llogs/ll_project/settings.py
+++ Llogs/ll_project/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'll_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates'],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
