1a01ca1baefd4b7f8d47c09e4fc9b4384412b563
Abdullahi <abkid04@gmail.com>
2026-03-01 12:58:53 +0100

    registration link

--- Llogs/accounts/views.py
+++ Llogs/accounts/views.py
@@ -15,6 +15,6 @@ def register(request):
             return redirect('learning_logs:index')
 # Display a blank or invalid form.
     context = {'form': form}
-    return render(request, 'registration/register.xhtml', context)
+    return render(request, 'register.xhtml', context)
 
 # Create your views here.
